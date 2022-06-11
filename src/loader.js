cfb3a26463b2bea4c6ee7036fc5496436489fb3a
Nick <104921681+DobriakovNick@users.noreply.github.com>
2022-06-11 18:10:34 +0200

    discord8

--- src/loader.js
+++ src/loader.js
@@ -3,26 +3,26 @@ const { Collection } = require('discord.js');
 
 client.commands = new Collection();
 
-const events = readdirSync('./події/').filter(file => file.endsWith('.js'));
+const events = readdirSync('./events/').filter(file => file.endsWith('.js'));
 
-console.log(`Завантаження подій ...`);
+console.log(`Loading events...`);
 
 for (const file of events) {
-    const event = require(`../подія/${file}`);
-    console.log(`-> Завантажив події ${file.split('.')[0]}`);
+    const event = require(`../events/${file}`);
+    console.log(`-> Loaded event ${file.split('.')[0]}`);
     client.on(file.split('.')[0], event.bind(null, client));
-    delete require.cache[require.resolve(`../подія/${file}`)];
+    delete require.cache[require.resolve(`../events/${file}`)];
 };
 
-console.log(`Завантаження команд ...`);
+console.log(`Loading commands...`);
 
-readdirSync('./команди/').forEach(dirs => {
-    const commands = readdirSync(`./команди/${dirs}`).filter(files => files.endsWith('.js'));
+readdirSync('./commands/').forEach(dirs => {
+    const commands = readdirSync(`./commands/${dirs}`).filter(files => files.endsWith('.js'));
 
     for (const file of commands) {
-        const command = require(`../команди/${dirs}/${file}`);
-        console.log(`-> Завантажив команди ${command.name.toLowerCase()}`);
+        const command = require(`../commands/${dirs}/${file}`);
+        console.log(`-> Loaded command ${command.name.toLowerCase()}`);
         client.commands.set(command.name.toLowerCase(), command);
-        delete require.cache[require.resolve(`../команди/${dirs}/${file}`)];
+        delete require.cache[require.resolve(`../commands/${dirs}/${file}`)];
     };
 });
